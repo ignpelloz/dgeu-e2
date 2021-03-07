fad330c3c26d207218a8befbab5220a892a3790b
Ignacio Peluaga <ignacio.peluaga.lozada@cern.ch>
2021-03-07 19:37:46 +0100

    Displaying results OK. Missing: fixing a bug with the json data and using redux

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -18,9 +18,10 @@ interface IFilterFormProps {
     uniText: string;
     wrapperItemCounter: string;
     */
-   uni: string
-   tipo: string
-   area: string
+    uni: string
+    tipo: string
+    area: string
+    results: [string, string][]
 }
 
 interface IFilterFormState {
@@ -34,6 +35,7 @@ interface IFilterFormState {
     uni: string
     tipo: string
     area: string
+    results: [string, string][]
 }
 
 
@@ -45,55 +47,61 @@ class FilterForm extends React.Component<{}, IFilterFormState> {
         this.state = {
             uni: "Universidad de Sevilla",
             tipo: "Máster",
-            area: "Ingeniería"
+            area: "Ingeniería",
+            results: []
         };
-        this.queryDB=this.queryDB.bind(this); // TODO: why needed?? https://stackoverflow.com/questions/45998744/react-this-state-is-undefined
+        this.queryDB = this.queryDB.bind(this); // TODO: why needed?? https://stackoverflow.com/questions/45998744/react-this-state-is-undefined
 
     }
 
-    public setUni(event:React.ChangeEvent<HTMLSelectElement>){
+    public setUni(event: React.ChangeEvent<HTMLSelectElement>) {
         let newState = {
             uni: event.target.value,
             tipo: this.state.tipo,
-            area: this.state.area
+            area: this.state.area,
+            results: this.state.results
         }
         this.setState(newState);
     }
-    public setArea(event:React.ChangeEvent<HTMLSelectElement>){
+    public setArea(event: React.ChangeEvent<HTMLSelectElement>) {
         let newState = {
             uni: this.state.uni,
             tipo: this.state.tipo,
-            area: event.target.value
+            area: event.target.value,
+            results: this.state.results
         }
         this.setState(newState);
     }
-    public setTipo(event:React.ChangeEvent<HTMLSelectElement>){
+    public setTipo(event: React.ChangeEvent<HTMLSelectElement>) {
         let newState = {
             uni: this.state.uni,
             tipo: event.target.value,
-            area: this.state.tipo
+            area: this.state.tipo,
+            results: this.state.results
         }
         this.setState(newState);
     }
 
 
     //public queryDB(uni: string, area: string, tipo: string){ // TODO: this should receive the 3 parameters and return a list of degrees (list of strings) that should be then rendered
-    public queryDB(){ // TODO: this should receive the 3 parameters and return a list of degrees (list of strings) that should be then rendered
-        
+    public queryDB() { // TODO: this should receive the 3 parameters and return a list of degrees (list of strings) that should be then rendered
+
         //uni=this.state."Universidad de Sevilla"
         //const area="Ingeniería"
         //area="Ciencias"
         //tipo="Máster"
-        
+
         //var res = []
-        const res:string[] = [];
-        for (var i = 0; i < data.length; i++){
-            
+        const res: [string, string][] = [];
+        for (var i = 0; i < data.length; i++) {
+
             var obj = data[i];
             //console.log(obj);
 
-            if (obj["university"] === this.state.uni){
-                
+            if (obj["university"] === this.state.uni) {
+
+                var uniWeb = obj["web"]
+
                 //console.log("")
                 //console.log(obj)
                 //console.log("")
@@ -101,31 +109,37 @@ class FilterForm extends React.Component<{}, IFilterFormState> {
                 var univ_degrees = obj["degrees"]
                 //console.log(univ_degrees)
 
-                for (var j = 0; j < univ_degrees.length; j++){
-                
+                for (var j = 0; j < univ_degrees.length; j++) {
+
                     //console.log(univ_degrees[j])
 
-                    if (this.state.tipo === "Máster" && univ_degrees[j]["name"].includes("Máster") && this.state.area === univ_degrees[j]["area"]){
-                        res.push(univ_degrees[j]["name"])
+                    if (this.state.tipo === "Máster" && univ_degrees[j]["name"].includes("Máster") && this.state.area === univ_degrees[j]["area"]) {
+                        res.push([univ_degrees[j]["name"], uniWeb])
                     }
-                    else if (this.state.tipo === "Grado" && !univ_degrees[j]["name"].includes("Máster") && this.state.area === univ_degrees[j]["area"]){
-                        res.push(univ_degrees[j]["name"])
+                    else if (this.state.tipo === "Grado" && !univ_degrees[j]["name"].includes("Máster") && this.state.area === univ_degrees[j]["area"]) {
+                        res.push([univ_degrees[j]["name"], uniWeb])
                     }
 
-                    
+
                 }
             }
         }
         console.log(res);
-        return res
+        let newState = {
+            uni: this.state.uni,
+            tipo: this.state.tipo,
+            area: this.state.area,
+            results: res
+        }
+        this.setState(newState);
     }
 
     public render() {
-        return ( 
-            
+        return (
+
             <div>
                 <h3>Buscador de Titulaciones</h3>
-            <div><label>Universidad </label>
+                <div><label>Universidad </label>
                     <select onChange={e => this.setUni(e)} >
                         <option value="Universidad de Sevilla">Universidad de Sevilla</option>
                         <option value="Universidad de Almería">Universidad de Almería</option>
@@ -135,25 +149,42 @@ class FilterForm extends React.Component<{}, IFilterFormState> {
                         <option value="Universidad de Málaga">Universidad de Málaga</option>
                         <option value="Universidad de Huelva">Universidad de Huelva</option>
                         <option value="Universidad de Jaén">Universidad de Jaén</option>
-                        <option value="Universidad Pablo de Olavide">Universidad Pablo de Olavide</option>                        
+                        <option value="Universidad Pablo de Olavide">Universidad Pablo de Olavide</option>
                     </select></div>
 
-                    <div style={{marginTop: "5px"}}><label>Area </label>
+                <div style={{ marginTop: "5px" }}><label>Area </label>
                     <select onChange={e => this.setArea(e)} >
                         <option value="Ingeniería">Ingenieria</option>
                         <option value="Ciencias">Ciencias</option>
                         <option value="Otros">Otras</option>
                     </select></div>
 
-                    <div style={{marginTop: "5px"}}><label>Tipo de Titulación</label>
+                <div style={{ marginTop: "5px" }}><label>Tipo de Titulación</label>
                     <select onChange={e => this.setTipo(e)} >
                         <option value="Máster">Máster</option>
                         <option value="Grado">Grado</option>
                     </select></div>
 
-                    <button style={{marginTop: "15px"}} onClick={this.queryDB} type="button">Buscar</button>
+                <button style={{ marginTop: "15px" }} onClick={this.queryDB} type="button">Buscar</button>
 
+                {/*
+                    <div style={{marginTop: "15px"}}>
+                        <p>{this.state.results}</p>
                     </div>
+                    */}
+
+                <div style={{ marginTop: "15px" }}>
+                    {this.state.results.map(degreeResult =>
+                        <div>
+                            <p>{degreeResult[0]} <a target="_blank" href={degreeResult[1]}>(Más Información)</a></p>
+                        </div>
+                    )}
+
+
+
+                </div>
+
+            </div>
         );
     }
 }
